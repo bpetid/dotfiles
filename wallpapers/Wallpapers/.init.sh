32a99f03eb960bba67717e84b58228b50c2197bf
Bányai Péter <bpetid@pm.me>
2022-09-24 19:33:46 +0200

    added most used packages

--- wallpapers/Wallpapers/.init.sh
+++ wallpapers/Wallpapers/.init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-pacman -S ttc-iosevka-ss08 neovim stow github-cli openssh 
+pacman -Syy --needed ttc-iosevka-ss08 neovim stow github-cli openssh mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
