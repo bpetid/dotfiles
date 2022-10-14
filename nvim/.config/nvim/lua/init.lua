370cfda3ad7136a20b6982abf7a969ec803e1e89
Bányai Péter <bpetid@pm.me>
2022-10-14 12:27:50 +0200

    Swap to zsh

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = { "c", "cpp", "lua", "rust", "python", "markdown" },
+	ensure_installed = { "c", "cpp", "lua", "rust", "python", "markdown", "toml" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = true,
