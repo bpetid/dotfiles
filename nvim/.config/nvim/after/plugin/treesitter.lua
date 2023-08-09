135c6a07e6c02a3399370d91048ffcd72504f5f2
Bányai Péter <bpeitd@pm.me>
2023-08-09 11:33:38 +0200

    Remove 'help' from treesitted default languages

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "arduino", "lua", "rust", "help", "cpp" },
+  ensure_installed = { "c", "arduino", "lua", "rust", "cpp" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
