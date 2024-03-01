d4b2e25d2dce38d28e0db79354af1feca6b58f1f
Bányai Péter <bpetid@pm.me>
2024-03-01 22:00:06 +0100

    Neovim config refresh

--- nvim/.config/nvim/lua/petid/plugins/lsp.lua
+++ nvim/.config/nvim/lua/petid/plugins/lsp.lua
@@ -24,7 +24,6 @@ return {
                 "bashls",
                 "cmake",
                 "jsonls",
-                "pylyzer",
                 "yamlls",
             },
 
--- nvim/.config/nvim/lua/petid/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/petid/plugins/treesitter.lua
@@ -7,10 +7,19 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "cpp", "arduino", "python" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "cpp", "c_sharp", "python", "perl", "ninja", "markdown_inline", "make", "gitignore", "gitcommit", "bash", "xml" },
             sync_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<leader>k", -- set to `false` to disable one of the mappings
+                    node_incremental = "<leader>l",
+                    scope_incremental = "<leader>,",
+                    node_decremental = "<leader>j",
+                },
+            },
         })
     end
 }
