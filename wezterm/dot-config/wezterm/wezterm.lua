e022a0498d5c1149a6a1893d16d982264d2eff53
Bányai Péter <bpetid@pm.me>
2025-08-16 22:18:24 +0200

    update wezterm config

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -3,53 +3,52 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window {}
-	window:gui_window():maximize()
-end)
+config.enable_wayland = true
+config.webgpu_power_preference = "LowPower"
+config.front_end = "WebGpu"
+config.log_unknown_escape_sequences = true
 
--- This is where you actually apply your config choices
-config.enable_wayland = false
 
 config.default_prog = { '/usr/bin/fish', '-l' }
 
-config.color_scheme = 'Tokyo Night Moon'
 
-config.harfbuzz_features = { 'calt', 'liga', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08',
-	'ss09' }
+config.color_scheme = 'Rosé Pine (base16)'
+
 
 config.font = wezterm.font_with_fallback {
-	'MonaspiceAr Nerd Font Mono',
-	'Symbols Nerd Font Mono',
-	'Noto Fonts Emoji'
+	{
+		family = 'Monaspace Argon',
+		weight = 'Light',
+		harfbuzz_features = {
+			'calt=1', 'liga=1', 'ss01=1', 'ss02=1', 'ss03=1', 'ss04=1', 'ss05=1', 'ss06=1', 'ss07=1', 'ss08=1', 'ss09=1'
+		},
+	},
+	{
+		family = 'Symbols Nerd Font Mono',
+	},
+	{
+		family = 'Noto Fonts Emoji',
+	}
 }
+config.font_size = 14.0
+config.freetype_load_target = 'Light'
+config.freetype_interpreter_version = 40
+config.freetype_load_flags = 'NO_AUTOHINT'
+config.allow_square_glyphs_to_overflow_width = 'Always'
 
-config.font_size = 12.0
--- config.freetype_load_target = 'Light'
 
 config.use_fancy_tab_bar = false
-
+config.hide_tab_bar_if_only_one_tab = false
 config.colors = {
 	tab_bar = {
-		-- The color of the strip that goes along the top of the window
-		-- (does not apply when fancy tab bar is in use)
-		background = '#0b0022',
-
-		-- The active tab is the one that has focus in the window
+		background = '#050802',
 		active_tab = {
-			-- The color of the background area for the tab
-			bg_color = '#2b2042',
-			-- The color of the text for the tab
+			bg_color = '#050802',
 			fg_color = '#c0c0c0',
-
-			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-			-- label shown for this tab.
-			-- The default is "Normal"
-			intensity = 'Normal',
+			intensity = 'Half',
 		}
 	}
 }
-
 config.initial_cols = 160
 config.initial_rows = 48
 
